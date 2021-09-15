a517904042033d9586bfcb7b8b80a0a9b9407c7d
Matt Brooks <mbrooks34@gsu.edu>
2021-09-15 09:50:56 -0400

    End of chapter 1.

---
+++ my-blog/src/pages/AboutPage.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+
+const AboutPage = () => (
+   <>
+      <h1>About Me</h1>
+      <p>Irure cupidatat labore consectetur ad labore irure elit est incididunt enim exercitation irure tempor. Sunt in consectetur nisi commodo fugiat magna. Cillum ut quis ut adipisicing eiusmod. Excepteur occaecat anim tempor deserunt aliquip.</p>
+
+      <p>Dolor ad culpa sint eiusmod minim cillum ipsum proident est enim commodo. Ea cillum laboris et voluptate reprehenderit consequat duis sint labore veniam Lorem consectetur nulla id. Adipisicing irure aliquip irure Lorem id laborum occaecat magna veniam eu mollit. Aliqua proident nostrud laboris ea in. Enim ea eiusmod dolore non. Minim magna consectetur commodo Lorem voluptate id elit incididunt voluptate. Do voluptate ea sit anim aliqua.</p>
+
+      <p>Esse sint nisi nisi aliqua id adipisicing laboris occaecat ea ea occaecat culpa. Aute ipsum eu consectetur esse minim proident sint esse. Mollit ea occaecat voluptate laboris velit sint. Sit excepteur cillum est id sunt consectetur officia nostrud sint exercitation nostrud mollit laborum. Do velit irure anim in. Labore ex veniam proident ut.</p>
+
+      <p>Ea sunt nostrud tempor aliqua do elit occaecat. Ad reprehenderit veniam dolore proident ad excepteur sunt adipisicing anim do sint labore reprehenderit est. Velit et occaecat est et tempor elit voluptate laboris occaecat. In anim ut duis culpa. Pariatur dolor voluptate minim eu tempor reprehenderit est ut.</p>
+   </>
+);
+
+
+export default AboutPage;
---
+++ my-blog/src/pages/ArticlesListPage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ArticlesList from '../components/ArticlesList';
+import articlesContent from './article-content';
+
+
+const ArticlesListPage = () => (
+   <>
+      <h1>Articles</h1>
+      <ArticlesList articles={articlesContent} />
+   </>
+);
+
+
+export default ArticlesListPage;
