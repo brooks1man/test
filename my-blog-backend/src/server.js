6e8b8cf00a44d03665dfb4b6878011662d591a23
Matt Brooks <mbrooks34@gsu.edu>
2021-09-28 10:56:15 -0400

    Ready for release.

--- my-blog-backend/src/server.js
+++ my-blog-backend/src/server.js
@@ -1,11 +1,12 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import { MongoClient } from 'mongodb';
+import path from 'path';
 
 
 const app = express();
 app.use(bodyParser.json());
-
+app.use(express.static(path.join(__dirname, '/build')));
 
 const withDB = async (operations, res) => {
    try {
@@ -61,6 +62,11 @@ app.post('/api/articles/:name/add-comment', (req, res) => {
 
       res.status(200).json(updatedArticleInfo);
    }, res);
+});
+
+
+app.get('*', (req, res) => {
+   res.sendFile(path.join(__dirname + '/build/index.html'));
 });
 
 
