daadb83ebd217c91e0cb7449120c48a3980d84ce
Matt Brooks <mbrooks34@gsu.edu>
2022-01-12 07:39:45 -0500

    Bootstrap 5 tutorial.

---
+++ bs5/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="utf-8" />
+   <meta name="viewport" content="width=device-width, initial-scale=1" />
+   <title>Bootstrap 5 Demo</title>
+
+   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js" integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf" crossorigin="anonymous"></script>
+   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous" />
+   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css" />
+</head>
+
+<body>
+   <main class="container mt-4">
+      <h1>BS5 Demo <i class="bi bi-emoji-smile text-primary"></i></h1>
+
+      <p class="lead">Ad aliquip in deserunt do est velit aliquip <a href="#" class="link-info">labore reprehenderit consectetur</a> sint eu labore aliqua. Ullamco ut aliqua et ea. Elit aliquip est et nostrud non nostrud et sit ipsum esse est. Ea esse voluptate incididunt dolore voluptate amet esse duis.</a>
+
+   </main>
+</body>
+</html>
